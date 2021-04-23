015726f108ea445befa75e103e1e989f133a034c
Satbek Abdyldayev <satbekmyrza@gmail.com>
2021-04-23 14:20:50 +0900

    initial commit

---
+++ include/llvm_seahorn/Transforms/Scalar.h
@@ -0,0 +1,13 @@
+#ifndef __LLVM_SEAHORN_TRANSFORMS_SCALAR__H_
+#define __LLVM_SEAHORN_TRANSFORMS_SCALAR__H_
+
+namespace llvm {class FunctionPass; class Pass;}
+namespace llvm_seahorn
+{
+  llvm::FunctionPass *createInstructionCombiningPass();
+  llvm::Pass* createNondetInitPass ();
+  llvm::Pass* createDeadNondetElimPass ();
+  llvm::Pass* createIndVarSimplifyPass ();
+  llvm::Pass* createFakeLatchExitPass ();
+}
+#endif
